62a4c955719ec4c0ebaa535de67f73ec3d17c4b8
xini <jade.rojas.navarro@alumnos.sip.cl>
2025-09-30 12:24:35 -0300

    t

--- base/__init__.py
+++ base/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect
 from datetime import datetime
 from base.controllers.citas import bp as citas_bp
 ## from base.controllers.planes import bp as planes_bp
@@ -9,16 +9,13 @@ def format_date(value, format='%Y-%m-%d'):
         value = datetime.strptime(value, '%Y-%m-%d')
     return value.strftime(format)
 
-def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static')
     app.config.from_mapping(
         SECRET_KEY='dev',
         DEBUG=True,
     )
     app.register_blueprint(citas_bp)
-    # app.register_blueprint(planes_bp)
     app.add_template_filter(format_date, 'format_date')
-
 
     @app.route('/')
     def index():
@@ -27,11 +24,18 @@ def create_app():
     @app.route('/dashboard')
     def dashboard():
         from base.models.cita_model import Cita
-        from flask import session, redirect
         if 'usuario_id' not in session:
             return redirect('/')
         citas = Cita.obtener_citas_usuarios(session['usuario_id'])
         return render_template('dashboard.html', citas=citas)
+
+    @app.route('/perfil')
+    def perfil():
+        if 'usuario_id' not in session:
+            return redirect('/')
+        from base.models.usuario_model import usuario
+        usuario_actual = usuario.obtener_por_id(session['usuario_id'])
+        return render_template('perfil.html', usuario=usuario_actual)
 
     return app
 
